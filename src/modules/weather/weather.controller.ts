b5ed5a2edceceb93d64d7710d7eb67ca215b5faf
SharpDevOps10 <td090833@gmail.com>
2025-05-16 17:50:34 +0300

    Add response for getWeather endpoint

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { WeatherService } from './weather.service';
+import { GetWeatherResponse } from './responses/get-weather.response';
 
 @Controller('weather')
 export class WeatherController {
   constructor (private readonly weatherService: WeatherService) {}
 
   @Get()
-  async getWeather (@Query('city') city: string) {
+  async getWeather (@Query('city') city: string): Promise<GetWeatherResponse> {
     return this.weatherService.getWeather(city);
   }
 }
