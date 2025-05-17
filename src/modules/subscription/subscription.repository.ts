8efb80079693d8bb4953d432cfad0d394d26fde9
SharpDevOps10 <td090833@gmail.com>
2025-05-17 15:12:38 +0300

    Add subscribe endpoint - add prisma configs - add Subscription entity - add Subscription repository - add basic subscribe logic

---
+++ src/modules/subscription/subscription.repository.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@database/prisma.service';
+import { CreateSubscriptionDto } from './dto/create-subscription.dto';
+import { Subscription } from '@prisma/client';
+import { ISubscriptionRepository } from './interfaces/subscription.repository.interface';
+
+@Injectable()
+export class SubscriptionRepository implements ISubscriptionRepository {
+  constructor (private readonly prismaService: PrismaService) {}
+
+  async findByEmailAndCity (email: string, city: string): Promise<Subscription> {
+    return this.prismaService.subscription.findFirst({
+      where: {
+        email,
+        city,
+      },
+    });
+  }
+
+  async createSubscription (data: CreateSubscriptionDto & { token: string }): Promise<void> {
+    await this.prismaService.subscription.create({
+      data: {
+        email: data.email,
+        city: data.city,
+        frequency: data.frequency,
+        token: data.token,
+        confirmed: false,
+      },
+    });
+  }
+}
